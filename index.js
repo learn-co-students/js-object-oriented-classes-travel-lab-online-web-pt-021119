bcc562cec54dbaf9b55939cc6744b5614ca09ee1
cyantis <cyantis@gmail.com>
2019-08-30 16:23:22 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+class Driver{
+  constructor(name, date){
+    this.name = name
+    this.date = date
+  }
 
+  startDate(){
+    return new Date(this.date);
+  }
+
+  yearsExperienceFromBeginningOf(endDate){
+    const startYear = this.startDate().getFullYear();
+    return endDate - startYear;
+  }
+}
+
+class Route{
+  constructor(){
+
+  }
+}
