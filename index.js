150df3b11b0711e48be6307d0b8ad8365b2bac64
cyantis <cyantis@gmail.com>
2019-08-30 18:11:21 -0600

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Driver{
   constructor(name, date){
-    this.name = name
-    this.date = date
+    this.name = name;
+    this.date = date;
   }
 
   startDate(){
@@ -16,7 +16,39 @@ class Driver{
 
 class Route{
   constructor(begginingLocation, endingLocation){
-    this.begginingLocation = begginingLocation
-    this.endingLocation = endingLocation
+    this.begginingLocation = begginingLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  routeConvert(street){
+    switch(street){
+      case 'Lexington':
+        return 4;
+        break;
+      case 'Park':
+        return 5;
+        break;
+      case 'Madison':
+        return 6;
+        break;
+      case '5th Avenue':
+        return 7;
+        break;
+      default:
+        return parseInt(street);
+    }
+  }
+
+  blocksTravelled(){
+    return Math.abs((this.routeConvert(this.begginingLocation.horizontal) - this.routeConvert(this.endingLocation.horizontal))) + Math.abs(this.begginingLocation.vertical - this.endingLocation.vertical);
+  }
+
+  estimatedTime(peak){
+    if(peak){
+      return this.blocksTravelled() / 2;
+    }
+    else {
+      return this.blocksTravelled() / 3;
+    }
   }
 }
