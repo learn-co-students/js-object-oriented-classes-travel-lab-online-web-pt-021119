3895daf709e821fd9f96e377a3757e0f35982874
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-09-16 06:41:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(endDate) {
+    return endDate - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+
+    let beginHorizontal = this.beginningLocation.horizontal;
+    let endHorizontal = this.endingLocation.horizontal;
+
+    let beginVertical = this.beginningLocation.vertical;
+    let endVertical = this.endingLocation.vertical;
+
+    let horizontalBlocks = Math.abs(eastWest.indexOf(endHorizontal) - eastWest.indexOf(beginHorizontal));
+    let verticalBlocks = Math.abs(parseInt(endVertical) - parseInt(beginVertical));
+
+    return horizontalBlocks + verticalBlocks;
+  }
+
+  estimatedTime() {
+    
+  }
+}
